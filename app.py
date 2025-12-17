114ce7438b42516365855dff995b5c5b8041e8fc
Taisei Ishiyama <issy0520.2@icloud.com>
2025-12-17 11:28:37 +0530

    test

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from caption_model import caption_image
+from translate_model import translate_en_to_ja
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # 仮：後で image_path はアップロードされた画像に置き換える
+    image_path = "static/uploads/dummy.jpg"
+    english = caption_image(image_path)
+    japanese = translate_en_to_ja(english)
+    return render_template("index.html", english_text=english, japanese_text=japanese)
 
-
-if __name__ =="__main__":
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
--- caption_model.py
+++ caption_model.py
@@ -1,1 +1,4 @@
 #caption_image(image_path: str) -> str
+
+def caption_image(image_path: str) -> str:
+    return "A dog playing with a ball"
